c27c35153594956ce6085637bbda9b08dde06d0e
supertgo <thiagoroberto2003@gmail.com>
2026-04-09 20:26:25 -0300

    7. Executando Todos os Testes

--- tests.py
+++ tests.py
@@ -145,7 +145,14 @@ class TestLoaderTest(TestCase):
 
 if __name__ == '__main__':
     loader = TestLoader()
-    suite = loader.make_suite(TestLoaderTest)
+    test_case_suite = loader.make_suite(TestCaseTest)
+    test_suite_suite = loader.make_suite(TestSuiteTest)
+    test_load_suite = loader.make_suite(TestLoaderTest)
+
+    suite = TestSuite()
+    suite.add_test(test_case_suite)
+    suite.add_test(test_suite_suite)
+    suite.add_test(test_load_suite)
 
     runner = TestRunner()
     runner.run(suite)
